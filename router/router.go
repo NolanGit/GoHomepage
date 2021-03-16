21dd42e654efd1113f4a3ee0481ee9ea813a8751
Nolan Sun <shr1213@live.com>
2021-03-16 22:02:51 +0800

    Create router.go

---
+++ router/router.go
@@ -0,0 +1,26 @@
+package router
+
+import (
+	"net/http"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+func SetupRouter() *gin.Engine {
+	router := gin.Default()
+	router.LoadHTMLGlob("./dist/index.html")
+	router.Static("/static", "./dist/static")
+	router.Use(cors.Default()) // 允许全部域名
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(
+			http.StatusOK,
+			"index.html",
+			gin.H{
+				"title": "Web",
+				"url":   "./web.json",
+			},
+		)
+	})
+	return router
+}
